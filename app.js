7ba62a7152a331a3cd6cb4d58f33c4e99eb76817
maceuge <maceuge@icloud.com>
2018-08-23 10:00:44 -0300

    Node Server etapa inicial, coneccion a mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Requieres
 var express = require('express');
+var mongoose = require('mongoose');
 
 // Inicializar Variables
 var app = express();
+
+// Conexion a la BD
+mongoose.connection.openUri('mongodb://localhost:27017/HospitalDB', (err, res) => {
+    if (err) throw err;
+    console.log('Base de Datos: \x1b[32m%s\x1b[0m', 'OnLine!');
+});
 
 // Rutas
 app.get('/', (req, res, next) => {
