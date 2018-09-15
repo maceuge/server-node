d078c228db98aacad84e6cd165d9418c7db5c949
Eugenio Vorontsov <maceuge@icloud.com>
2018-09-15 12:30:06 -0300

    Se configuro el puerto para heroku

--- app.js
+++ app.js
@@ -75,6 +75,6 @@ app.use('/', appRoutes);
 
 
 // Escuchar peticiones
-server.listen(3000, () => {
+server.listen( process.env.PORT || 3000, () => {
     console.log('Express Server Puerto: 3000 \x1b[32m%s\x1b[0m', 'OnLine!');
 });
